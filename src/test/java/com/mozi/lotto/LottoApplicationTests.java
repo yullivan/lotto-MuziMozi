a53553037e067190fbc71c7e0ff2d835d6eed45a
Mozi <seung5727@naver.com>
2024-12-10 13:43:41 +0900

    초기 커밋

---
+++ src/test/java/com/mozi/lotto/LottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozi.lotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
